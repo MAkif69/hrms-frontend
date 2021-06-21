3667d64a4c32e6893dd7aac0436af0f85110c7b2
M.Akif <M.Akif@DESKTOP-HHK6G12>
2021-06-21 12:18:40 +0300

    JobPostings getAll Succesfully. Day 11 completed. Some refactor will make.

--- src/pages/JobPostings.jsx
+++ src/pages/JobPostings.jsx
@@ -1,56 +1,39 @@
-import React from "react";
-import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import React, { useState, useEffect } from "react";
+import { Table } from "semantic-ui-react";
+import JobPostingService from "../services/JobPostingService";
 
 export default function JobPostings() {
+  const [jobPostings, setjobPostings] = useState([]);
+
+  useEffect(() => {
+    let jobPostingService = new JobPostingService();
+    jobPostingService.getJobPostings().then(result=>setjobPostings(result.data.data))
+  });
+
   return (
     <div>
       <Table celled>
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell>Header</Table.HeaderCell>
-            <Table.HeaderCell>Header</Table.HeaderCell>
-            <Table.HeaderCell>Header</Table.HeaderCell>
+            <Table.HeaderCell>Şirket Adı</Table.HeaderCell>
+            <Table.HeaderCell>Pozisyon</Table.HeaderCell>
+            <Table.HeaderCell>Alınacak Kişi Sayısı</Table.HeaderCell>
+            <Table.HeaderCell>İlana Çıkış Tarihi</Table.HeaderCell>
+            <Table.HeaderCell>Son Başvuru Tarihi</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
 
         <Table.Body>
-          <Table.Row>
-            <Table.Cell>
-              <Label >First</Label>
-            </Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-          </Table.Row>
+          {jobPostings.map((jobPost) => (
+            <Table.Row key={jobPost.id}>
+              <Table.Cell>{jobPost.companyName}</Table.Cell>
+              <Table.Cell>{jobPost.positionName}</Table.Cell>
+              <Table.Cell>{jobPost.openPositionNumber}</Table.Cell>
+              <Table.Cell>{jobPost.createdDate}</Table.Cell>
+              <Table.Cell>{jobPost.lastApplyDate}</Table.Cell>
+            </Table.Row>
+          ))}
         </Table.Body>
-
-        <Table.Footer>
-          <Table.Row>
-            <Table.HeaderCell colSpan="3">
-              <Menu floated="right" pagination>
-                <Menu.Item as="a" icon>
-                  <Icon name="chevron left" />
-                </Menu.Item>
-                <Menu.Item as="a">1</Menu.Item>
-                <Menu.Item as="a">2</Menu.Item>
-                <Menu.Item as="a">3</Menu.Item>
-                <Menu.Item as="a">4</Menu.Item>
-                <Menu.Item as="a" icon>
-                  <Icon name="chevron right" />
-                </Menu.Item>
-              </Menu>
-            </Table.HeaderCell>
-          </Table.Row>
-        </Table.Footer>
       </Table>
     </div>
   );
